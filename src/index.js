0af3dfbcd364d442cbf97e7e6f38aa2aae5b2d75
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2020-11-20 20:28:17 -0800

    added config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
+//import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+let awsconfig = {
+    Auth: {
+        identityPoolId: "us-east-1:a130f516-f9b7-4d0b-82dd-25e248b49511",
+        region: "us-east-1",
+        userPoolId:  "us-east-1_CALDr4EZA",
+        userPoolWebClientId: "79gsrp64vm7vrdh6bk0j8fl20j",
+        oauth: {
+            domain:"http://newtechcorpauth-dev.auth.us-east-1.amazoncognito.com/",
+            scope: ['email', 'openid'],
+            redirectSignIn: "https://master.d36s98jndilz0p.amplifyapp.com/",
+            redirectSignOut: "https://master.d36s98jndilz0p.amplifyapp.com/",
+            responseType: "code"
+        }
+      },
+
+      aws_appsync_graphqlEndpoint:  "https://wznhygavpfbgdgsy7oibu2g2sq.appsync-api.us-east-1.amazonaws.com/graphql",
+      aws_appsync_region: "us-east-1",
+      aws_appsync_authenticationType:"AMAZON_COGNITO_USER_POOLS"
+}
+if (isLocalhost) {
+  awsconfig.Auth.oauth.redirectSignIn = 'http://localhost:3001/';
+  awsconfig.Auth.oauth.redirectSignOut = 'http://localhost:3001/';
+}
+
+Amplify.configure(awsconfig);
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
